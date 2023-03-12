828ed9fd93b0b6b19cdfc8700e2dfcfa9f9697eb
W-Novytskyi <113388494+W-Novytskyi@users.noreply.github.com>
2023-03-12 15:28:24 +0200

    зміна юрл

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,9 +2,8 @@ import { Notify } from 'notiflix';
 export { fetchCountries };
 
 function fetchCountries(name) {
-    return fetch(
-        `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flag,languages`
-        ).then(response => {
+    const url = `https://restcountries.com/v2/name/${name}?fields=name,capital,population,flag,languages`;
+    return fetch(url).then(response => {
             if (!response.ok) {
                 return Notify.failure(
                     'Oops, there is no country with that name.'
